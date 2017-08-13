6ba09e2124656d31be40fd7c0b55e24fb453083f
Gskartwii <gskw+git@crescentcode.net>
2017-08-13 20:04:04 +0300

    Fix errors being thrown with successful 0x0B parsing

--- Packet83_0B.go
+++ Packet83_0B.go
@@ -1,6 +1,7 @@
 package main
 import "github.com/google/gopacket"
 import "errors"
+import "io"
 
 type Packet83_0B struct {
 	Instances []*ReplicationInstance
@@ -31,6 +32,9 @@ func DecodePacket83_0B(thisBitstream *ExtendedReader, context *CommunicationCont
 	var i uint32
 	for i = 0; i < arrayLen; i++ {
 		layer.Instances[i], err = DecodeReplicationInstance(true, gzipStream, context, packet, instanceSchema)
+		if arrayLen - i == 1 && err == io.EOF {
+			return layer, nil // What is up with Go's gzip streams?
+		}
 		if err != nil {
 			return layer, err
 		}
