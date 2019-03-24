08c44c977c2beff8c7969a52930dea3311102168
GSKW <gskw+git@crescentcode.net>
2019-03-24 15:57:29 +0200

    [peer/parser] Minor cleanup

--- peer/RakNetLayer.go
+++ peer/RakNetLayer.go
@@ -10,7 +10,21 @@ import (
 	"log"
 	"net"
 	"strings"
+
+	bitstream "github.com/gskartwii/go-bitstream"
 )
+
+func bufferToStream(buffer []byte) *extendedReader {
+	return &extendedReader{bitstream.NewReader(bytes.NewReader(buffer))}
+}
+
+func bitsToBytes(bits uint) uint {
+	return (bits + 7) >> 3
+}
+
+func bytesToBits(bytes uint) uint {
+	return bytes << 3
+}
 
 // DEBUG decides whether debug mode should be on or not.
 const DEBUG bool = true
