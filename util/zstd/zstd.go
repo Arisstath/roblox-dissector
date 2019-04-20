5da60b8eb056ffcc1db05b54fe191f34c6d4fdfa
GSKW <gskw+git@crescentcode.net>
2019-04-20 14:25:19 +0300

    [other] Add util for zstd debugging

---
+++ util/zstd/zstd.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"flag"
+	"io"
+	"os"
+
+	"github.com/DataDog/zstd"
+)
+
+func main() {
+	isDecompress := flag.Bool("d", false, "If set, will decompress instead of compress")
+	outFileName := flag.String("o", "", "Path to output file")
+	flag.Parse()
+	inFileName := flag.Arg(0)
+
+	var err error
+	var inputFile *os.File
+	var inputStream io.ReadCloser
+	var outputFile *os.File
+	var outputStream io.WriteCloser
+
+	if inFileName == "" || *outFileName == "" {
+		flag.Usage()
+		return
+	}
+
+	inputFile, err = os.Open(inFileName)
+	if err != nil {
+		panic(err)
+	}
+	outputFile, err = os.OpenFile(*outFileName, os.O_CREATE|os.O_WRONLY, 0777)
+	if err != nil {
+		panic(err)
+	}
+
+	if *isDecompress {
+		println("Will decompress")
+		inputStream = zstd.NewReader(inputFile)
+		outputStream = outputFile
+	} else {
+		println("Will compress")
+		inputStream = inputFile
+		outputStream = zstd.NewWriter(outputFile)
+	}
+
+	_, err = io.Copy(outputStream, inputStream)
+	if err != nil {
+		panic(err)
+	}
+
+	err = inputFile.Close()
+	if err != nil {
+		panic(err)
+	}
+	err = outputFile.Close()
+	if err != nil {
+		panic(err)
+	}
+
+	println("Successfully completed operation")
+}
