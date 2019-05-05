2708df311e8f7a20b64ddc2752e6f195f429989e
GSKW <gskw+git@crescentcode.net>
2019-05-05 11:26:14 +0300

    [parser] Fix PingPacketLayers.go lint errors

--- peer/PingPacketLayers.go
+++ peer/PingPacketLayers.go
@@ -1,13 +1,9 @@
 package peer
 
-// ID_CONNECTED_PING - client <-> server
+// Packet00Layer represents ID_CONNECTED_PING - client <-> server
 type Packet00Layer struct {
 	// Timestamp (seconds)
 	SendPingTime uint64
-}
-
-func NewPacket00Layer() *Packet00Layer {
-	return &Packet00Layer{}
 }
 
 func (thisStream *extendedReader) DecodePacket00Layer(reader PacketReader, layers *PacketLayers) (RakNetPacket, error) {
@@ -18,6 +14,8 @@ func (thisStream *extendedReader) DecodePacket00Layer(reader PacketReader, layer
 
 	return layer, err
 }
+
+// Serialize implements RakNetPacket.Serialize()
 func (layer *Packet00Layer) Serialize(writer PacketWriter, stream *extendedWriter) error {
 	var err error
 	err = stream.WriteByte(0)
@@ -30,23 +28,23 @@ func (layer *Packet00Layer) Serialize(writer PacketWriter, stream *extendedWrite
 func (layer *Packet00Layer) String() string {
 	return "ID_CONNECTED_PING"
 }
+
+// TypeString implements RakNetPacket.TypeString()
 func (Packet00Layer) TypeString() string {
 	return "ID_CONNECTED_PING"
 }
+
+// Type implements RakNetPacket.Type()
 func (Packet00Layer) Type() byte {
 	return 0
 }
 
-// ID_CONNECTED_PONG - client <-> server
+// Packet03Layer represents ID_CONNECTED_PONG - client <-> server
 type Packet03Layer struct {
 	// Timestamp from ID_CONNECTED_PING
 	SendPingTime uint64
 	// Timestamp of reply (seconds)
 	SendPongTime uint64
-}
-
-func NewPacket03Layer() *Packet03Layer {
-	return &Packet03Layer{}
 }
 
 func (thisStream *extendedReader) DecodePacket03Layer(reader PacketReader, layers *PacketLayers) (RakNetPacket, error) {
@@ -62,6 +60,7 @@ func (thisStream *extendedReader) DecodePacket03Layer(reader PacketReader, layer
 	return layer, err
 }
 
+// Serialize implements RakNetPacket.Serialize()
 func (layer *Packet03Layer) Serialize(writer PacketWriter, stream *extendedWriter) error {
 	var err error
 	err = stream.WriteByte(3)
@@ -79,9 +78,13 @@ func (layer *Packet03Layer) Serialize(writer PacketWriter, stream *extendedWrite
 func (layer *Packet03Layer) String() string {
 	return "ID_CONNECTED_PONG"
 }
+
+// TypeString implements RakNetPacket.TypeString()
 func (Packet03Layer) TypeString() string {
 	return "ID_CONNECTED_PONG"
 }
+
+// Type implements RakNetPacket.Type()
 func (Packet03Layer) Type() byte {
 	return 3
 }
