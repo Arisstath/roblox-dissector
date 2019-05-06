03b8f57246e816a6910928d7287f821b49d9edab
GSKW <gskw+git@crescentcode.net>
2019-05-06 19:25:48 +0300

    [gui] Add base for Lumikide processing (not released)

---
+++ NoLumikide.go
@@ -0,0 +1,14 @@
+// +build !lumikide
+
+package main
+
+import (
+	"github.com/Gskartwii/roblox-dissector/peer"
+	"github.com/therecipe/qt/widgets"
+)
+
+const LumikideEnabled = false
+
+func LumikideProcessContext(parent widgets.QWidget_ITF, context *peer.CommunicationContext) error {
+	widgets.QMessageBox_Critical(parent, "Lumikide disabled", "Lumikide disabled at compile time, sorry!", widgets.QMessageBox__Ok, widgets.QMessageBox__Ok)
+}
