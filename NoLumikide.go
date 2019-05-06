867575a23f12a3568f3e05e5b430779fba89ad7e
GSKW <gskw+git@crescentcode.net>
2019-05-06 19:27:12 +0300

    [gui] Fix NoLumikide arguments

--- NoLumikide.go
+++ NoLumikide.go
@@ -9,6 +9,6 @@ import (
 
 const LumikideEnabled = false
 
-func LumikideProcessContext(parent widgets.QWidget_ITF, context *peer.CommunicationContext) error {
+func LumikideProcessContext(parent widgets.QWidget_ITF, context *peer.CommunicationContext, rbxfileDataModel *rbxfile.Root) error {
 	widgets.QMessageBox_Critical(parent, "Lumikide disabled", "Lumikide disabled at compile time, sorry!", widgets.QMessageBox__Ok, widgets.QMessageBox__Ok)
 }
