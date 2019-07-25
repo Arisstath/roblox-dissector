ccde846e05709a840ed202e629260b48735a7880
Arisstath <me@arisstath.me>
2019-07-25 03:04:52 +0300

    updated security settings for latest roblox

--- peer/Client.go
+++ peer/Client.go
@@ -482,27 +482,13 @@ func (settings *windows10SecuritySettings) GenerateIDResponse(challenge uint32) 
 	return 0x70D0B0BC - challenge
 }
 func (settings *windows10SecuritySettings) GenerateTicketHash(ticket string) uint32 {
-	var ecxHash uint32
 	initHash := xxHash32.Checksum([]byte(ticket), 1)
-	initHash += 0x557BB5D7
-	initHash = bits.RotateLeft32(initHash, 0x07)
-	initHash -= 0x443921D5
-	initHash *= 0x443921D5
-	initHash = bits.RotateLeft32(initHash, 0x0D)
-	ecxHash = 0x557BB5D7 - initHash
-	ecxHash ^= 0x443921D5
-	ecxHash = bits.RotateLeft32(ecxHash, -0x11)
-	ecxHash -= 0x11429402
-	ecxHash = bits.RotateLeft32(ecxHash, 0x17)
-	initHash = ecxHash + 0x11429402
-	initHash = bits.RotateLeft32(initHash, 0x1D)
-	initHash ^= 0x443921D5
-	initHash = -initHash
+	result := -(bits.RotateLeft32(bits.RotateLeft32(bits.RotateLeft32((0x443921D5- bits.RotateLeft32(0x557BB5D7 * (bits.RotateLeft32(initHash + 0x557BB5D7, 7) - 0x557BB5D7), 0xD)) ^ 0x557BB5D7, 0x11)+ 0x664B2854,0x17) - 0x664B2854,-29) ^ 0x557BB5D7);
 
 	return initHash
 }
 func (settings *windows10SecuritySettings) PatchTicketPacket(packet *Packet8ALayer) {
-	packet.SecurityKey = "2e427f51c4dab762fe9e3471c6cfa1650841723b!61dc7a7733d638d7815461897f5ba4e1\x0E"
+	packet.SecurityKey = "2e427f51c4dab762fe9e3471c6cfa1650841723b!b503184b1a41087f9124e287c1d1729b\x0E"
 	packet.GoldenHash = 0xC001CAFE
 	packet.DataModelHash = "ios,ios"
 	packet.Platform = settings.osPlatform
