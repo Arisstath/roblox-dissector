7dbc3cc54811b36e39eb24e664697a37d42b60e2
Gskartwii <gskw+git@crescentcode.net>
2019-08-22 15:29:56 +0300

    [peer] Update Luau challenge test

--- peer/LuauChallenge_test.go
+++ peer/LuauChallenge_test.go
@@ -3,8 +3,8 @@ package peer
 import "testing"
 
 func TestLuauChallenge(t *testing.T) {
-	var expected int32 = 5553188
-	var result = ResolveLuaChallenge(1215435312, 250190180)
+	var expected int32 = 1890828
+	var result = ResolveLuaChallenge(-0x1558389F, -0x75320513)
 	if result != expected {
 		t.Fatalf("%08X != %08X", result, expected)
 	}
