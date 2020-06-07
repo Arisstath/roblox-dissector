ffea5b5042e1d235b0d0e2283b8680184d95d9c6
GSKW <gskartwii@gmail.com>
2020-06-07 13:39:04 +0300

    [gui] Fix build without WinDivert

--- NoWinDivert.go
+++ NoWinDivert.go
@@ -1,9 +1,11 @@
 // +build !divert
-
 package main
+
+import "errors"
 
 const WinDivertEnabled = false
 
-func (session *CaptureSession) CaptureFromDivert(_, _ string) {
+func (session *CaptureSession) CaptureFromWinDivert() error {
 	// nop
+	return errors.New("windivert disabled at build time")
 }
