2930496ba81039439e33c182dbfc4f57ef6b7066
Gskartwii <ahannula4@gmail.com>
2020-07-08 17:41:47 +0300

    [gui] Implement ReliabilityLayer displaying in PacketDetailsViewer
    This also adds a visualizer for the received split packets.

--- PacketDetailsViewer.go
+++ PacketDetailsViewer.go
@@ -2,19 +2,37 @@ package main
 
 import (
 	"encoding/hex"
+	"fmt"
 	"io/ioutil"
+	"math"
+	"strconv"
 
 	"github.com/Gskartwii/roblox-dissector/peer"
+	"github.com/gotk3/gotk3/cairo"
 	"github.com/gotk3/gotk3/gdk"
 	"github.com/gotk3/gotk3/gtk"
 )
 
+type splitPacketRange struct {
+    start uint32
+    end uint32 // inclusive
+}
+
 type PacketDetailsViewer struct {
 	mainWidget *gtk.Notebook
 	logBox     *gtk.TextView
-	hexBox     *gtk.TextView
 
+	hexBox     *gtk.TextView
 	hexDumpData []byte
+
+	reliablity *gtk.Label
+	rmNumber *gtk.Label
+	channel *gtk.Label
+	index *gtk.Label
+	splitPacketsLabel *gtk.Label
+
+	missingSplitPacketRanges []splitPacketRange
+	countSplitPackets uint32
 }
 
 func NewPacketDetailsViewer() (*PacketDetailsViewer, error) {
@@ -118,14 +136,87 @@ func NewPacketDetailsViewer() (*PacketDetailsViewer, error) {
 		}
 	})
 
+	reliability_, err := builder.GetObject("reliabilitytype")
+	if err != nil {
+    	return nil, err
+	}
+	reliability, ok := reliability_.(*gtk.Label)
+	if !ok {
+    	return nil, invalidUi("reliabilitytype")
+	}
+	rmNumber_, err := builder.GetObject("rmnumber")
+	if err != nil {
+    	return nil, err
+	}
+	rmNumber, ok := rmNumber_.(*gtk.Label)
+	if !ok {
+    	return nil, invalidUi("rmnumber")
+	}
+	channel_, err := builder.GetObject("channel")
+	if err != nil {
+    	return nil, err
+	}
+	channel, ok := channel_.(*gtk.Label)
+	if !ok {
+    	return nil, invalidUi("channel")
+	}
+	index_, err := builder.GetObject("orderingidx")
+	if err != nil {
+    	return nil, err
+	}
+	index, ok := index_.(*gtk.Label)
+	if !ok {
+    	return nil, invalidUi("orderingidx")
+	}
+	splitPacketsLabel_, err := builder.GetObject("splitpacketslabel")
+	if err != nil {
+    	return nil, err
+	}
+	splitPacketsLabel, ok := splitPacketsLabel_.(*gtk.Label)
+	if !ok {
+    	return nil, invalidUi("splitpacketslabel")
+	}
+
+	drawingArea_, err := builder.GetObject("splitpacketsdrawarea")
+	if err != nil {
+    	return nil, err
+	}
+	drawingArea, ok := drawingArea_.(*gtk.DrawingArea)
+	if !ok {
+    	return nil, invalidUi("drawingarea")
+	}
+	drawingArea.Connect("draw", func(drawingArea *gtk.DrawingArea, ctx *cairo.Context) bool {
+        viewWidth := float64(drawingArea.GetAllocatedWidth())
+        viewHeight := float64(drawingArea.GetAllocatedHeight())
+		ctx.Rectangle(0, 0, viewWidth, viewHeight)
+		ctx.SetSourceRGB(46.0/255.0, 125.0/255.0, 50.0/255.0)
+		ctx.Fill()
+
+		ctx.SetSourceRGB(198.0/255.0, 40.0/255.0, 40.0/255.0)
+		count := float64(viewer.countSplitPackets)
+		for _, range_ := range viewer.missingSplitPacketRanges {
+			startCoord := float64(range_.start) / count * viewWidth
+			width := math.Ceil(float64(range_.end - range_.start) / count) * viewWidth
+			ctx.Rectangle(startCoord, 0, width, viewHeight)
+			ctx.Fill()
+		}
+
+		return false
+	})
+
 	viewer.mainWidget = notebook
 	viewer.logBox = logBox
 	viewer.hexBox = hexBox
+	viewer.reliablity = reliability
+	viewer.rmNumber = rmNumber
+	viewer.channel = channel
+	viewer.index = index
+	viewer.splitPacketsLabel = splitPacketsLabel
 
 	return viewer, nil
 }
 
-func (viewer *PacketDetailsViewer) ShowPacket(layers *peer.PacketLayers) error {
+func (viewer *PacketDetailsViewer) updateHexTab(layers *peer.PacketLayers) error {
 	if layers.OfflinePayload != nil {
 		viewer.hexDumpData = layers.OfflinePayload
 	} else {
@@ -137,6 +228,73 @@ func (viewer *PacketDetailsViewer) ShowPacket(layers *peer.PacketLayers) error {
 	}
 	textBuffer.SetText(hex.Dump(viewer.hexDumpData))
 	viewer.hexBox.SetBuffer(textBuffer)
+	return nil
+}
+
+var reliabilityNames = []string{
+	"Unreliable",
+	"Unreliable, sequenced",
+	"Reliable",
+	"Reliable, ordered",
+	"Reliable, sequenced",
+}
+
+func (viewer *PacketDetailsViewer) updateReliabilityTab(layers *peer.PacketLayers) error {
+	if layers.OfflinePayload != nil {
+		viewer.missingSplitPacketRanges = nil
+		viewer.reliablity.SetText("N/A")
+		viewer.rmNumber.SetText("N/A")
+		viewer.channel.SetText("N/A")
+		viewer.index.SetText("N/A")
+		viewer.splitPacketsLabel.SetText("Split packets (N/A):")
+	} else {
+    	viewer.reliablity.SetText(reliabilityNames[layers.Reliability.Reliability])
+    	if layers.Reliability.IsReliable() {
+			viewer.rmNumber.SetText(strconv.FormatInt(int64(layers.Reliability.ReliableMessageNumber), 10))
+    	} else {
+        	viewer.rmNumber.SetText("N/A")
+    	}
+    	if layers.Reliability.IsOrdered() {
+			viewer.channel.SetText(strconv.FormatInt(int64(layers.Reliability.OrderingChannel), 10))
+			viewer.index.SetText(strconv.FormatInt(int64(layers.Reliability.OrderingIndex), 10))
+    	} else {
+        	viewer.channel.SetText("N/A")
+        	viewer.index.SetText("N/A")
+    	}
+
+		viewer.countSplitPackets = layers.Reliability.SplitPacketCount
+    	viewer.missingSplitPacketRanges = nil
+    	received := 0
+    	for i := uint32(0); i < viewer.countSplitPackets; i++ {
+        	if layers.SplitPacket.ReliablePackets[i] == nil {
+            	rangeCount := len(viewer.missingSplitPacketRanges)
+				if rangeCount == 0 || viewer.missingSplitPacketRanges[rangeCount - 1].end != i - 1 {
+    				viewer.missingSplitPacketRanges = append(viewer.missingSplitPacketRanges, splitPacketRange{
+                         start: i,
+                         end: i,
+    				})
+				} else {
+    				viewer.missingSplitPacketRanges[rangeCount - 1].end++
+				}
+        	} else {
+            	received++
+        	}
+    	}
+    	viewer.splitPacketsLabel.SetText(fmt.Sprintf("Split packets (%d/%d):", received, viewer.countSplitPackets))
+	}
+
+	return nil
+}
+
+func (viewer *PacketDetailsViewer) ShowPacket(layers *peer.PacketLayers) error {
+	err := viewer.updateHexTab(layers)
+	if err != nil {
+    	return err
+	}
+	err = viewer.updateReliabilityTab(layers)
+	if err != nil {
+    	return err
+	}
 
 	return nil
 }
