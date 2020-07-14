98694aa85da4e1d28ae089d44dbab52c4dfdf863
Aleksi Hannula <ahannula4@gmail.com>
2020-07-14 14:01:57 +0300

    [gui] Add profiler option

--- roblox-dissector.go
+++ roblox-dissector.go
@@ -2,9 +2,17 @@ package main
 
 import (
 	"github.com/gotk3/gotk3/gtk"
+	"net/http"
+	_ "net/http/pprof"
+	"os"
 )
 
 func main() {
+	if len(os.Args) > 1 && os.Args[1] == "-profile" {
+		go func() {
+			println(http.ListenAndServe("localhost:6060", nil))
+		}()
+	}
 	gtk.Init(nil)
 
 	settings, err := gtk.SettingsGetDefault()
