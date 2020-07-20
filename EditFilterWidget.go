e78eee30bc2166fef12a01dfe3dd78312d959a09
Aleksi Hannula <ahannula4@gmail.com>
2020-07-20 20:29:01 +0300

    [gui] Add button to load filter from file

--- EditFilterWidget.go
+++ EditFilterWidget.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+    "io/ioutil"
+
 	"github.com/therecipe/qt/core"
 	"github.com/therecipe/qt/gui"
 	"github.com/therecipe/qt/widgets"
@@ -24,14 +26,32 @@ func NewEditFilterWidget(parent widgets.QWidget_ITF, oldFilter string, oldUseExt
 	filterInput.SetPlainText(oldFilter)
 	layout.AddWidget(filterInput, 0, 0)
 
+	buttonRow := widgets.NewQHBoxLayout()
+	buttonRow.SetAlignment(core.QtAlign__Left)
+
 	okButton := widgets.NewQPushButton2("Apply", nil)
-	layout.AddWidget(okButton, 0, 0)
+	buttonRow.AddWidget(okButton, 0, 0)
 	okButton.ConnectReleased(func() {
 		filterScript := filterInput.ToPlainText()
 		useExtraInfo := extraInfo.CheckState() == core.Qt__Checked
 		window.Close()
 		callback(filterScript, useExtraInfo)
 	})
+	loadFromFileButton := widgets.NewQPushButton2("Open file...", nil)
+	buttonRow.AddWidget(loadFromFileButton)
+	loadFromFileButton.ConnectReleased(func() {
+		file := widgets.QFileDialog_GetOpenFileName(window, "Open filter script file", "", "Lua filter scripts (*.lua)", "", 0)
+		if file != "" {
+			script, err := ioutil.ReadFile(file)
+			if err != nil {
+    			println("failed to open file:", err.Error())
+    			return
+			}
+			filterInput.SetPlainText(string(script))
+		}
+	})
+
+	layout.AddWidget(buttonRow, 0, 0)
 
 	window.SetLayout(layout)
 	window.Show()
